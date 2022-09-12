d3b1e4d7b0aac74b7511a1513e23ea8313fa8644
YuukiAnimeSensei <icchan417@gmail.com>
2022-09-12 20:40:17 +0900

    prefectureからmessageに変更

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const main = async () => {
     const supabaseUrl = 'https://djrtvirahriyqlyhovhx.supabase.co'
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqcnR2aXJhaHJpeXFseWhvdmh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE3NzM5NjQsImV4cCI6MTk3NzM0OTk2NH0.RNYgQPzUqk7YGMEXVxfjOAAbOCzpYZSmGBdT5qbHOq4'
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqcnR2aXJhaHJpeXFseWhvdmh4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2MTc3Mzk2NCwiZXhwIjoxOTc3MzQ5OTY0fQ.s3XIObIagKdAEGqrRnfSv5zTshIDGSqwApuTf_S6g8M'
     const client = supabase.createClient(supabaseUrl, supabaseKey)
     console.log(client);
     //SELECT prefecture, city from imairu ORDER BY created_at desc LIMIT 1;  
@@ -11,7 +11,7 @@ const main = async () => {
         .limit(1)
     console.log(imairu);
     const place = imairu[0]
-    const msg = `${place.prefecture}${place.city}に滞在中です`
+    const msg = `${place.message}`
     document.querySelector("#msg").innerText = msg
 
 }
